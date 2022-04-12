5b7a0614dedde1d1a10e00e3a367bca7b073d872
Night <94296856+ccsswap@users.noreply.github.com>
2022-04-12 22:14:28 +0200

    Add files via upload

---
+++ Webhook Violator/run to setup.bat
@@ -0,0 +1,8 @@
+@echo off
+python -m pip install pystyle==1.1
+python -m pip install requests==2.27.1
+python -m pip install os
+python -m pip install dhooks
+python -m pip install time
+python -m pip install requests
+python -m pip install colorama
